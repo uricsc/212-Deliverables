c14887d35827f33e7e5e446cf5a62e2695d3d8c0
Jonathan S <111279098+js-uri@users.noreply.github.com>
2023-09-05 17:07:25 -0400

    Update main_2.cpp

--- Labs/Lab 1 C++ Crash Course/lab_01_scaffolding/main_2.cpp
+++ Labs/Lab 1 C++ Crash Course/lab_01_scaffolding/main_2.cpp
@@ -6,28 +6,65 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
+Animal* AnimalFactory(std::string type, float hunger, float thirst);
 
 int main(int argc, char*argv[]){
-
     // Extract # of animals we're expecting to read from the CLA
+    int num_animals = std::stoi(argv[1]);
+    // Extract the
+    std::string fname(argv[2]);
+    std::ifstream input_file(fname);
 
-    // Extract the name of the file we'll be reading from the CLA
+    std::vector<Animal*> animal_arr;
 
-	// Create a collection (data structure) of the appropriate size to hold the # of animals we are creating
-	
-	// Loop through the number of animals we have.
-		
-		// Read the current line from the file
-		
-		// Tokenize the line into type, hunger, thirst
-		
-		// Based on the type, create the appropriate animal and add it to your collection of animals
-		
-	// Loop through your collection of animals
-	
-		// Speak
-		
-		// Output hunger
-		
-		// Output thirst
+    std::string current_line;
+    // For use with tokenizing
+    std::string temp;
+    std::string curr_type;
+    float curr_hunger;
+    float curr_thirst;
+
+    // Loop through the number of animals we have.
+    for(int i = 0; i < num_animals; i++){
+        // Read the current line from the file
+        std::getline(input_file, current_line);
+        // Convert the current line into a stream
+        std::stringstream current_line_stream(current_line);
+
+        // Copy the type directly into our 'type' variable
+        std::getline(current_line_stream, curr_type, ' ');
+
+        // Copy the hunger value into your temp variable
+        std::getline(current_line_stream, temp, ' ');
+        // Convert our temp variable into a float & store into 'curr_hunger'
+        curr_hunger = std::stof(temp);
+
+        // Copy the thirst value into your temp variable
+        std::getline(current_line_stream, temp, ' ');
+        // Convert our temp variable into a float & store into 'curr_thirst'
+        curr_thirst = std::stof(temp);
+
+        animal_arr.push_back(AnimalFactory(curr_type, curr_hunger, curr_thirst));
+    }
+
+    for(int i = 0; i < num_animals; i++){
+        animal_arr[i]->Speak();
+        std::cout << animal_arr[i]->GetHunger() << std::endl;
+        std::cout << animal_arr[i]->GetThirst() << std::endl;
+    }
+}
+
+Animal* AnimalFactory(std::string type, float hunger, float thirst){
+    if(type == "Cat"){
+        return new Cat(hunger, thirst);
+    }else if(type == "Dog"){
+        return new Dog(hunger, thirst);
+    }else if(type == "Bird"){
+        return new Bird(hunger, thirst);
+    }else if(type == "Hamster"){
+        return new Hamster(hunger, thirst);
+    }else if(type == "Snake"){
+        return new Snake(hunger, thirst);
+    }
 }
