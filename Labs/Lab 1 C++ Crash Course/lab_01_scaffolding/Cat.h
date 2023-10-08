1f7286a999fee03142522870ba8e6d6654b0c20e
Jonathan S <111279098+js-uri@users.noreply.github.com>
2023-10-08 06:25:05 -0400

    Delete Labs/Lab 1 C++ Crash Course directory

--- Labs/Lab 1 C++ Crash Course/lab_01_scaffolding/Cat.h
+++
@@ -1,7 +0,0 @@
-#include "Animal.h"
-
-class Cat : public Animal{
-public:
-    Cat(float initial_hunger) : Animal(initial_hunger){};
-    void Speak() { std::cout << "Meow!" << std::endl; }
-};
--- Labs/Lab 1 C++ Crash Course/lab_01_scaffolding/Dog.h
+++
@@ -1,7 +0,0 @@
-#include "Animal.h"
-
-class Dog : public Animal{
-public:
-    Dog(float initial_hunger) : Animal(initial_hunger){};
-    void Speak() { std::cout << "Woof!" << std::endl; }
-};
